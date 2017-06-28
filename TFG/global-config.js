492f39744da522da9d4644c6ed0c3c4e6e34ba81
elenamoron <93elena@hotmail.es>
2017-06-28 10:09:05 +0200

    js de config

--- TFG/global-config.js
+++ TFG/global-config.js
@@ -1,7 +1,5 @@
-<script>
-     var GlobalConfig = {
+var GlobalConfig = {
         access_token: 'Bearer a98988f7bdece40715673967ffa874cd11fee1f2c1b67d1a07983983effbd442',
         api_host: 'https://prueba.com',
         api_url_base: '/TFG/elena'
     };
-</script>
