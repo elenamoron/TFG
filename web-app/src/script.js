908984f9817522242c4e582790f039e62b032e1f
elena <e.moron@i2tic.com>
2017-09-12 12:02:46 +0200

    Formato de código

--- web-app/src/script.js
+++ web-app/src/script.js
@@ -1,22 +1,22 @@
 (function (document) {
-'use strict';
-document.addEventListener('WebComponentsReady', function() {
+    'use strict';
+    document.addEventListener('WebComponentsReady', function() {
 
-    // We have to bind the template with the model
-    var t = document.querySelector('#t');
-    var ajaxRequest = t.$.dataAjax;
+        // We have to bind the template with the model
+        var t = document.querySelector('#t');
+        var ajaxRequest = t.$.dataAjax;
 
-    // make the iron-ajax call
-    t.postData = function() {
-      ajaxRequest.body = {
-        'text': t.dataToPost
-      } 
-      ajaxRequest.generateRequest();
-    }
+        // make the iron-ajax call
+        t.postData = function() {
+            ajaxRequest.body = {
+                'text': t.dataToPost
+            }
+            ajaxRequest.generateRequest();
+        }
 
-    //callback on request complete
-    t.postComplete = function(){
-      alert('whoa! request complete');
-    }
-});
+        //callback on request complete
+        t.postComplete = function(){
+            alert('whoa! request complete');
+        }
+    });
 });(document);
