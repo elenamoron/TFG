0f0798537eacb68bd6c02107b8c72a02eabfd39a
elena <e.moron@i2tic.com>
2018-01-17 17:59:41 +0100

    Mostrar tablas en el admin django

--- backend/lpbc/admin.py
+++ backend/lpbc/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Perfil, Organization
 # Register your models here.
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('created','name', 'code')
+
+
+admin.site.register(Organization, OrganizationAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nombre', 'apellidos', 'telefono', 'mail', 'codigo_postal', 'observaciones')
+    search_fields = ('user__username','nombre','apellidos')
+
+admin.site.register(Perfil, ProfileAdmin)
