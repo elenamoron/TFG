412655a1ea4cbf42f8cb1acce50a4989ae0aa6c5
elenamoron <93elena@hotmail.es>
2018-01-18 22:14:57 +0100

    Tablas del admin django

--- backend/lpbc/admin.py
+++ backend/lpbc/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Perfil, Organization
+from .models import Perfil, Organization, Proyecto, PersonaFisica, PersonaJuridica, Relacion, Documento, RelacionJuridicaFisica
 # Register your models here.
 
 class OrganizationAdmin(admin.ModelAdmin):
@@ -13,3 +13,37 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__username','nombre','apellidos')
 
 admin.site.register(Perfil, ProfileAdmin)
+
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'fecha_creacion')
+
+admin.site.register(Proyecto, ProyectoAdmin)
+
+class PersonaJuridicaAdmin(admin.ModelAdmin):
+    list_display = ('denominacion_social', 'CIF', 'ubicacion', 'fecha_constitucion', 'sector', 'forma_juridica', 'registro',
+        'numero_inscripciones', 'poblacion', 'provincia', 'codigo_postal', 'pais', 'telefono', 'email')
+
+admin.site.register(PersonaJuridica,PersonaJuridicaAdmin)
+
+class PersonaFisicaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo', 'documento_identificativo', 'fecha_caducidad', 'nacionalidad', 'pais_nacionalidad',
+        'lugar_nacimiento', 'pais_residencia', 'domicilio', 'telefono', 'email', 'acreditacion_poderes')
+
+admin.site.register(PersonaFisica,PersonaFisicaAdmin)
+
+class RelacionAdmin(admin.ModelAdmin):
+    list_display = ('relacion')
+
+admin.site.register(Relacion, RelacionAdmin)
+
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display('meta_descripcion', 'blog')
+
+admin.site.register(Documento, DocumentoAdmin)
+
+class RelacionJuridicaFisicaAdmin(admin.ModelAdmin):
+    list_display('persona_juridica', 'persona_fisica', 'tipo_relacion', 'datos_particulares')
+    #documentos = models.ManyToOneRel(Documento)
+
+admin.site.register(RelacionJuridicaFisica, RelacionJuridicaFisicaAdmin)
+
