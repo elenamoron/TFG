77df0fd118720c3703b0c3845227f1d8ec9116f2
elena <e.moron@i2tic.com>
2018-05-17 09:55:11 +0200

    Correciones en representantes

--- lpbc/app/organization/serializers.py
+++ lpbc/app/organization/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
 from organization.models import Organization, Profile, Project
 
+
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = ('id', 'name','description', 'code')
+        fields = ('id', 'name', 'description', 'code')
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('id', 'user', 'nombre', 'apellidos', 'fecha_nacimiento', 'codigo_postal', 'telefono', 'mail', 'observaciones')
+        fields = ('id', 'user', 'nombre', 'apellidos', 'fecha_nacimiento', 'codigo_postal', 'telefono', 'mail',
+                  'observaciones')
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'name','description', 'fecha_creacion', 'cliente', 'logo','activo')
+        fields = ('id', 'name', 'description', 'fecha_creacion', 'cliente', 'logo', 'activo')
